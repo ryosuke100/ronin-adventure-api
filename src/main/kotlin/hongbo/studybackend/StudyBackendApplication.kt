a4d5c06797623226b5d34dec65d16ff37fc2321e
Ryosuke <ando.ryosuke1@gmail.com>
2024-01-05 11:01:45 -0500

    [RONIN-TECH][Ryosuke] init: init project

---
+++ src/main/kotlin/hongbo/studybackend/StudyBackendApplication.kt
@@ -0,0 +1,11 @@
+package hongbo.studybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyBackendApplication>(*args)
+}
---
+++ src/test/kotlin/hongbo/studybackend/StudyBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package hongbo.studybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
